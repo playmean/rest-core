84d693d0504e77a3ca9a7d1c3abfb7efbfae3fa4
playmean <playmean.book@gmail.com>
2021-10-23 22:45:38 +0300

    feat: model abstract

---
+++ model-set.go
@@ -0,0 +1,67 @@
+package restcore
+
+import "reflect"
+
+type ModelSet struct {
+	original   interface{}
+	fieldNames map[string]string
+}
+
+func NewModelSet(input interface{}) *ModelSet {
+	m := &ModelSet{
+		original:   input,
+		fieldNames: make(map[string]string),
+	}
+
+	m.resolveNames()
+
+	return m
+}
+
+func (m *ModelSet) GetNames() ([]string, []string) {
+	names := make([]string, 0, len(m.fieldNames))
+	dbNames := make([]string, 0, len(m.fieldNames))
+
+	for name, dbName := range m.fieldNames {
+		names = append(names, name)
+		dbNames = append(dbNames, dbName)
+	}
+
+	return names, dbNames
+}
+
+func (m *ModelSet) GetValues() map[string]interface{} {
+	dbValues := make(map[string]interface{})
+
+	pointer := reflect.Indirect(reflect.ValueOf(m.original))
+
+	for structName, dbName := range m.fieldNames {
+		field := pointer.FieldByName(structName)
+
+		if field.IsZero() {
+			continue
+		}
+
+		dbValues[dbName] = field.Interface()
+	}
+
+	return dbValues
+}
+
+func (m *ModelSet) resolveNames() {
+	el := reflect.TypeOf(m.original).Elem()
+
+	el.FieldByNameFunc(func(name string) bool {
+		field, _ := el.FieldByName(name)
+
+		dbName := field.Tag.Get("db")
+
+		if dbName == "" || dbName == "-" {
+			return false
+		}
+
+		m.fieldNames[name] = dbName
+
+		return false
+	})
+}
---
+++ model.go
@@ -0,0 +1,169 @@
+package restcore
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+type Model struct {
+	Database  *sqlx.DB `json:"-"`
+	TableName string   `json:"-"`
+
+	original interface{}
+	set      *ModelSet
+}
+
+type ModelSortOptions struct {
+	Field     string
+	Direction string
+}
+
+type ModelLoadOptions struct {
+	Where   map[string]interface{}
+	Sort    *ModelSortOptions
+	GroupBy string
+}
+
+func (m *Model) ModelInitialize(db *sqlx.DB, tableName string, input interface{}) {
+	m.Database = db
+	m.TableName = tableName
+
+	m.original = input
+	m.set = NewModelSet(input)
+}
+
+func (m *Model) ModelLoadByID(id string) error {
+	err := m.Database.Get(m.original, fmt.Sprintf(`
+		SELECT * FROM %s WHERE id = ?
+	`, m.TableName), id)
+	if err != nil {
+		return NewApiError(&ApiErrorOptions{
+			Code:     "SELECT",
+			Subcode:  m.TableName,
+			Message:  fmt.Sprintf("%s with id '%s' not found", m.TableName, id),
+			Original: err,
+		})
+	}
+
+	return nil
+}
+
+func (m *Model) ModelLoad(opts ModelLoadOptions) error {
+	var sql = fmt.Sprintf("SELECT * FROM %s WHERE ", m.TableName)
+	var values []interface{}
+
+	for k, v := range opts.Where {
+		sql += fmt.Sprintf("%s = ? AND ", k)
+		values = append(values, v)
+	}
+
+	sql += "1"
+
+	if opts.Sort != nil {
+		sql += fmt.Sprintf(" ORDER BY %s %s", opts.Sort.Field, opts.Sort.Direction)
+	}
+
+	if len(opts.GroupBy) > 0 {
+		sql += fmt.Sprintf(" GROUP BY %s", opts.GroupBy)
+	}
+
+	err := m.Database.Get(m.original, sql, values...)
+	if err != nil {
+		return NewApiError(&ApiErrorOptions{
+			Code:     "SELECT",
+			Subcode:  m.TableName,
+			Message:  fmt.Sprintf("%s not found", m.TableName),
+			Original: err,
+		})
+	}
+
+	return nil
+}
+
+func (m *Model) ModelCreate() error {
+	sqlQuery := fmt.Sprintf("INSERT INTO %s", m.TableName)
+
+	dbNames, values := m.getValues()
+
+	dbNames = append(dbNames, "id")
+	values = append(values, uuid.Must(uuid.NewV4()).String())
+
+	placeholders := strings.Repeat("?, ", len(dbNames)-1) + "?"
+
+	sqlQuery += fmt.Sprintf("(%s) VALUES (%s)", strings.Join(dbNames, ", "), placeholders)
+
+	_, err := m.Database.Exec(sqlQuery, values...)
+	if err != nil {
+		return NewApiError(&ApiErrorOptions{
+			Code:     "INSERT",
+			Subcode:  m.TableName,
+			Message:  "cannot insert row",
+			Original: err,
+		})
+	}
+
+	return nil
+}
+
+func (m *Model) ModelSave() error {
+	sqlQuery := fmt.Sprintf("UPDATE %s SET ", m.TableName)
+
+	assigns, values, idValue := m.getAssigns()
+
+	values = append(values, idValue)
+
+	sqlQuery += fmt.Sprintf("%s WHERE id = ?", strings.Join(assigns, ", "))
+
+	_, err := m.Database.Exec(sqlQuery, values...)
+	if err != nil {
+		return NewApiError(&ApiErrorOptions{
+			Code:     "UPDATE",
+			Subcode:  m.TableName,
+			Message:  fmt.Sprintf("cannot update row with id '%s'", idValue),
+			Original: err,
+		})
+	}
+
+	return nil
+}
+
+func (m *Model) getValues() ([]string, []interface{}) {
+	dbNames := make([]string, 0)
+	values := make([]interface{}, 0)
+
+	for dbName, value := range m.set.GetValues() {
+		dbNames = append(dbNames, dbName)
+		values = append(values, value)
+	}
+
+	return dbNames, values
+}
+
+func (m *Model) getAssigns() ([]string, []interface{}, string) {
+	assigns := make([]string, 0)
+	values := make([]interface{}, 0)
+
+	var idValue string = ""
+
+	for dbName, value := range m.set.GetValues() {
+		if dbName == "id" {
+			idValue = value.(reflect.Value).String()
+
+			continue
+		}
+
+		if dbName == "updated" {
+			value = uint(time.Now().Unix())
+		}
+
+		assigns = append(assigns, fmt.Sprintf("%s = ?", dbName))
+		values = append(values, value)
+	}
+
+	return assigns, values, idValue
+}
